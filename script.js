fcc7fa767347f4a27ecbb45c6624e221e763f2db
Sonia Craciun <bysoniiaaa@gmail.com>
2022-02-03 16:58:40 -0800

    added functions for city input, search-button, clear history, city name, current pic, temperature, humidity, wind-speed, UV index, history, and search, and also when the search button is clicked you can read city name by the user

---
+++ script.js
@@ -0,0 +1,24 @@
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");4
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+    
+
+    const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
+    //  When search button is clicked, read the city name typed by the user
+
+    function getWeather(cityName) {
+//  Using saved city name, execute a current condition get request from open weather map api
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        axios.get(queryURL)
+        .then(function(response){
+            console.log(response);
