e381ef8bfa171e94e4a11cfecb79ab8ac966dc7e
Sonia Craciun <bysoniiaaa@gmail.com>
2022-02-03 18:02:22 -0800

    added the response to display current conditions

--- script.js
+++ script.js
@@ -22,3 +22,33 @@ function initPage() {
         axios.get(queryURL)
         .then(function(response){
             console.log(response);
+//  Parse response to display current conditions
+    //  Method for using "date" objects obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+        const currentDate = new Date(response.data.dt*1000);
+        console.log(currentDate);
+        const day = currentDate.getDate();
+        const month = currentDate.getMonth() + 1;
+        const year = currentDate.getFullYear();
+        nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+        let weatherPic = response.data.weather[0].icon;
+        currentPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+        currentPicEl.setAttribute("alt",response.data.weather[0].description);
+        currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+        currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+        currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+    let lat = response.data.coord.lat;
+    let lon = response.data.coord.lon;
+    let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+    axios.get(UVQueryURL)
+    .then(function(response){
+        let UVIndex = document.createElement("span");
+        UVIndex.setAttribute("class","badge badge-danger");
+        UVIndex.innerHTML = response.data[0].value;
+        currentUVEl.innerHTML = "UV Index: ";
+        currentUVEl.append(UVIndex);
+    });
+    //  Using saved city name, execute a 5-day forecast get request from open weather map api
+    let cityID = response.data.id;
+    let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+    axios.get(forecastQueryURL)
+    .then(function(response){
